20d68faa2ec6f3d859a1c21913599fa5b492374b
viges2201 <viges2201@hotmail.com>
2018-06-14 00:53:38 +0300

    Add connection DataBase

--- src/main/java/bl/Util.java
+++ src/main/java/bl/Util.java
@@ -1,4 +1,25 @@
 package bl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static final String DB_Driver = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+
+    public Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName(DB_Driver);
+            connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+            System.out.println("Connection is success");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection is lose");
+        }
+        return connection;
+    }
 }
